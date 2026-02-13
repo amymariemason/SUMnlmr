404f3289bafc88d11c5d17ee96122b38fbd70a7d
Amy Mason~ <amymariemason@gmail.com>
2026-02-13 16:02:44 +0000

    added tests of new functionality

--- tests/testthat/test-create_nlmr_summary.R
+++ tests/testthat/test-create_nlmr_summary.R
@@ -92,3 +92,80 @@ test_that("throws errors", {
                                              strata_method = "ranked"))
 
 })
+
+test_that("supports custom stratification inputs", {
+  set.seed(1234)
+  test_data <- generated_data
+  e_cov <- as.matrix(test_data$u)
+
+  summ_xs <- create_nlmr_summary(
+    y = test_data$log.Y,
+    x = test_data$X,
+    g = test_data$g,
+    q = 10,
+    family = "gaussian",
+    strata_method = "ranked",
+    xs = test_data$X + test_data$u,
+    seed = 1234
+  )
+
+  expect_length(summ_xs$strata, length(test_data$X))
+  expect_true(all(!is.na(summ_xs$strata)))
+
+  expect_snapshot_output(create_nlmr_summary(create_nlmr_summary(
+    y = test_data$log.Y,
+    x = test_data$X,
+    g = test_data$g,
+    q = 10,
+    family = "gaussian",
+    strata_method = "ranked",
+    xs = test_data$X + test_data$u,
+    seed = 1234
+  )))
+
+
+}
+)
+
+
+test_that("creates custom stratification inputs", {
+
+  set.seed(1234)
+  test_data <- generated_data
+  e_cov <- as.matrix(test_data$u)
+  e_interaction <- as.matrix(test_data$u^2)
+
+  summ_method2 <- create_nlmr_summary(
+    y = test_data$log.Y,
+    x = test_data$X,
+    g = test_data$g,
+    q = 10,
+    family = "gaussian",
+    strata_method = "ranked",
+    xs_covar = e_cov,
+    xs_interaction = e_interaction,
+    include_standard_output = TRUE,
+    seed = 1234
+  )
+
+  expect_true("summary_standard" %in% names(summ_method2))
+  expect_true("strata_standard" %in% names(summ_method2))
+  expect_equal(nrow(summ_method2$summary_standard), 10)
+  expect_length(summ_method2$strata_standard, length(test_data$X))
+
+
+  expect_snapshot_output(create_nlmr_summary(create_nlmr_summary(
+    y = test_data$log.Y,
+    x = test_data$X,
+    g = test_data$g,
+    q = 10,
+    family = "gaussian",
+    strata_method = "ranked",
+    xs_covar = e_cov,
+    xs_interaction = e_interaction,
+    include_standard_output = TRUE,
+    seed = 1234
+  )))
+
+})
+
